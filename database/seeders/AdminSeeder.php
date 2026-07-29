5516263ca2b1f434da3d83f38e56be26ddebf380
Khairul122 <khairulhuda242@gmail.com>
2026-07-29 21:00:54 +0700

    feat: add AdminSeeder and configure log directory ignore rules

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,7 +11,7 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'admin@edusmart.com'],
+            ['email' => 'admin@gmail.com'],
             [
                 'name' => 'Administrator',
                 'username' => 'admin',
